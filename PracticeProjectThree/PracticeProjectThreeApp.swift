1020fae664655962425d4d12bffa1720ceefbede
Rahul Avale <rahulavale9@gmail.com>
2025-01-17 08:26:59 -0500

    Initial Commit

---
+++ PracticeProjectThree/PracticeProjectThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectThreeApp.swift
+//  PracticeProjectThree
+//
+//  Created by Rahul Avale on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
