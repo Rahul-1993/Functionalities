56cf6119ceccf5d5252870904bdd589c59b79d90
Rahul Avale <rahulavale9@gmail.com>
2025-03-27 19:05:40 -0400

    Add property wrapper view

---
+++ PracticeProjectThree/Preview Content/Views/Funtionalities/PropertyWrappers/PropertyWrappersView.swift
@@ -0,0 +1,52 @@
+//
+//  PropertyWrappersView.swift
+//  PracticeProjectThree
+//
+//  Created by Rahul Avale on 3/24/25.
+//
+
+import SwiftUI
+
+struct PropertyWrappersList: Identifiable {
+    let id: Int
+    let propertyName: String
+    let propertyView: AnyView
+    let propertyImage: String
+}
+
+class PropertyWrappersViewModel: ObservableObject {
+    let propertyList : [PropertyWrappersList] = [
+        PropertyWrappersList(id: 1, propertyName: "App Storage", propertyView: AnyView(AppStorageView()), propertyImage: "swift")
+    ]
+}
+
+struct PropertyWrappersView: View {
+    
+    @StateObject private var viewModel = PropertyWrappersViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                RadialGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                               center: .topLeading, startRadius: 5, endRadius: UIScreen.main.bounds.height)
+                .ignoresSafeArea()
+                
+                List(viewModel.propertyList) { data in
+                    NavigationLink {
+                        data.propertyView
+                    } label: {
+                        Label(data.propertyName, systemImage: data.propertyImage)
+                    }
+                }
+                .scrollContentBackground(.hidden)
+            }
+            .navigationTitle("Property Wrappers")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    PropertyWrappersView()
+}
